64dba229cd7d6cff93b5c4723e88623ed40f3394
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-17 23:06:08 +0530

    [Hareesh]

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,35 +7,41 @@
             Console.WriteLine("welcome to the snake and ladder program!");
             int PlayerPosition = 0;
             Console.WriteLine("singleplayer position is now starting as zero");
-            int playerNumberOfRolls = 0; 
+            int playerNumberOfRolls = 0;
             Random random = new Random();
-
-            int playerDiceRoll = random.Next(1, 7);
-            playerNumberOfRolls++;
-
-            Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
-            int checkOptions = random.Next(0, 3);
-            switch (checkOptions)
+            while (PlayerPosition < 100)
             {
-                case 0:
-                    Console.WriteLine("No play");
-                    break;
-                case 1:
-                    PlayerPosition += playerDiceRoll;
-                    Console.WriteLine("player position moves a head");
-                    break;
-                case 2:
-                    PlayerPosition -= playerDiceRoll;
-                    Console.WriteLine("player position moves a behind");
-                    break;
-            }
-            if (PlayerPosition < 0)
-            {
-                PlayerPosition = 0;
-            }
+                int playerDiceRoll = random.Next(1, 7);
+                playerNumberOfRolls++;
 
-            Console.WriteLine("the position of player now  " + PlayerPosition);
+                Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
+                int checkOptions = random.Next(0, 3);
+                switch (checkOptions)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        break;
+                    case 1:
+                        PlayerPosition += playerDiceRoll;
+                        Console.WriteLine("player position moves a head");
+                        break;
+                    case 2:
+                        PlayerPosition -= playerDiceRoll;
+                        Console.WriteLine("player position moves a behind");
+                        break;
+                }
+                if (PlayerPosition < 0)
+                {
+                    PlayerPosition = 0;
+                }
+                if (PlayerPosition > 100)
+                {
+                    PlayerPosition = 100;
+                }
 
+                Console.WriteLine("the position of player now  " + PlayerPosition);
+
+            }
         }
     }
 }
