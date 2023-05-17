14d8e04073637d63847351fafd43bd0673eb95c8
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-17 22:29:52 +0530

    [Hareesh]

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -14,6 +14,23 @@
             playerNumberOfRolls++;
 
             Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
+            int checkOptions = random.Next(0, 3);
+            switch (checkOptions)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    PlayerPosition += playerDiceRoll;
+                    Console.WriteLine("player position moves a head");
+                    break;
+                case 2:
+                    PlayerPosition -= playerDiceRoll;
+                    Console.WriteLine("player position moves a behind");
+                    break;
+
+            }
+            Console.WriteLine("the position of player now  " + PlayerPosition);
         }
     }
 }
